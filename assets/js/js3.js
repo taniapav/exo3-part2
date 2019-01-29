60fef390ddbdc4af94400e0084342dfa055d9505
Tatjana <pavlovic.tatjana.p@gmail.com>
2019-01-29 12:06:42 +0100

    exo3 part2

---
+++ assets/js/js3.js
@@ -0,0 +1,7 @@
+function clavier(){
+  // on ajout le .value pour recuperer  la valeur d'un élément.
+
+   var lastname = document.getElementById('lastname').value;
+
+   alert('Nom : ' + lastname);
+}
